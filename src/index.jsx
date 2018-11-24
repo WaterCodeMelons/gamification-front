6cc32de94aa882c653990164a2b72a0cf87a3f38
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2018-11-24 23:30:05 +0100

    feat(SCSS): SCSS support - Added node-sass - Added some initial style directory boilerplate

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './config/serviceWorker';
 import { App } from './app/app.component';
 
+import './index.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
