89abdae83197f02dd640c81518d94da92c73120e
Dawid Yerginyan <dawid.yerginyan@gmail.com>
2019-01-05 20:11:12 +0100

    prettier fix

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Redirect,
   Route,
-  Switch
+  Switch,
 } from 'react-router-dom';
 import { Dashboard } from './+dashboard/dashboard.component';
 import { Login } from './+login/login.component';
